f15608ddd8562cf30cfb5649798886e252f34830
Kungfoowiz <kungfoowiz@hotmail.com>
2024-11-18 16:52:11 +0000

    Create IGreetSvc.cs

---
+++ IGreetSvc.cs
@@ -0,0 +1,10 @@
+namespace ExampleDDD.Service
+{
+
+    // This service must implement a Greet method that takes an input string of name.
+    public interface IGreetSvc
+    {
+        string Greet(string name);
+    }
+
+}
